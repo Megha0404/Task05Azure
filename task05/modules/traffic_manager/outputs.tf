1ab2dbd2e22b1c872e6818c53e7cb13d73d3bd01
Megha0404 <megha_kumari1@epam.com>
2025-05-16 11:48:48 +0530

    Removed all descriptions from outputs.tf

--- task05/modules/traffic_manager/outputs.tf
+++ task05/modules/traffic_manager/outputs.tf
@@ -1,4 +1,4 @@
 output "fqdn" {
   value = azurerm_traffic_manager_profile.this.fqdn
-  #description = "The number of outputs meets the requirements. All outputs have descriptions"
+
 }
--- task05/outputs.tf
+++ task05/outputs.tf
@@ -3,14 +3,5 @@ output "traffic_manager_fqdn" {
   description = "The number of outputs meets the requirements. All outputs have descriptions"
 }
 
-/*output "resource_groups" {
-  value       = module.resource_groups
-  description = "The number of outputs meets the requirements. All outputs have descriptions"
-}
 
-output "app_service_plans" {
-  value       = module.app_service_plans
-  description = "The number of outputs meets the requirements. All outputs have descriptions"
-}
-*/
 
